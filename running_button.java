0bb68f0dff93ba869a19aeaad49ffd08dbc1349e
vaniadze314 <230211@student.pwr.edu.pl>
2019-03-09 23:39:58 +0100

    update AdjFrame class

--- running_button.java
+++ running_button.java
@@ -2,18 +2,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Dimension;
+import java.awt.GraphicsDevice.WindowTranslucency;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Color;
 import java.awt.Event;
+import java.awt.Color;
 import java.util.Random;
-
+import java.lang.Math;
 
 
 class AdjFrame extends JFrame implements ActionListener{
 
-    private int width, height;
+    private int width, height, btn_x=80, btn_y=50;
     private JButton btn;
     private JPanel panel;
     private Random pos_generator;
@@ -22,10 +23,13 @@ class AdjFrame extends JFrame implements ActionListener{
 
     @Override 
     public void actionPerformed(ActionEvent e) {
-        int x,y;
-        x = pos_generator.nextInt(width);
-        y = pos_generator.nextInt(height);
-        btn.setLocation(x,y);
+        int x_temp = pos_generator.nextInt();
+        int y_temp = pos_generator.nextInt();
+        int x = Math.abs(x_temp%(width-btn_x));
+        int y = Math.abs(y_temp%(height-btn_y));
+        System.out.println(x+" "+y);
+        btn.setLocation(x,y);  
+        repaint();     
     }
 
     AdjFrame(){
@@ -37,12 +41,16 @@ class AdjFrame extends JFrame implements ActionListener{
      btn = new JButton("Exit");
      btn.addActionListener(this);
      panel.add(btn);
-     btn.setPreferredSize(new Dimension(80, 50));
+     btn.setPreferredSize(new Dimension(btn_x, btn_y));
      setSize(width,height);
      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     setTitle("Running Button"); 
+     setTitle("Running Button");    
      getContentPane().add(panel);
+     setUndecorated(true);
      setVisible(true);
+     panel.setBackground(new Color(0,0,0,0));
+     btn.setBackground(new Color(255,0,0));
+     
     }
 }
 
