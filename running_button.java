74804e68829e108ff7f1a51317d02b09e6682db3
vaniadze314 <230211@student.pwr.edu.pl>
2019-03-09 21:31:24 +0100

    update AdjFrame class

--- running_button.java
+++ running_button.java
@@ -1,17 +1,48 @@
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Event;
+import java.util.Random;
 
-class AdjFrame extends JFrame{
+
+
+class AdjFrame extends JFrame implements ActionListener{
+
+    private int width, height;
+    private JButton btn;
+    private JPanel panel;
+    private Random pos_generator;
+
+
+
+    @Override 
+    public void actionPerformed(ActionEvent e) {
+        int x,y;
+        x = pos_generator.nextInt(width);
+        y = pos_generator.nextInt(height);
+        btn.setLocation(x,y);
+    }
+
     AdjFrame(){
      Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-     int width = (int)dim.getWidth();
-     int height = (int) dim.getHeight();
-     //System.out.println("Width="+(int)width+"\n"+"Height="+(int)height);
+     width = (int)dim.getWidth();
+     height = (int) dim.getHeight();
+     pos_generator = new Random();
+     panel = new JPanel();
+     btn = new JButton("Exit");
+     btn.addActionListener(this);
+     panel.add(btn);
+     btn.setPreferredSize(new Dimension(80, 50));
      setSize(width,height);
      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     setTitle("Running Button"); 
+     getContentPane().add(panel);
      setVisible(true);
-     setTitle("Running Button");
     }
 }
 
